8fc176dfbd844366f21a2ef1a298aed6608c211a
ejaakml <eximindotong@yahoo.com>
2020-11-07 16:53:02 +0700

    31

---
+++ src/pboif2/pkg10119061/laatihan31/perkenalanmahasiswa/Mahasiswa.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.laatihan31.perkenalanmahasiswa;
+
+/**
+ *
+ * @author Rheiza
+ */
+class Mahasiswa {
+  String nim;
+    String nama;
+    
+    public void perkenalkanDiri() {
+    System.out.println("Hello Everyone");
+    System.out.println("My NIM is " + nim);
+    System.out.println("My Name is " + nama);
+    System.out.println();
+    }
+}
---
+++ src/pboif2/pkg10119061/laatihan31/perkenalanmahasiswa/PBOIF210119061Laatihan31PerkenalanMahasiswa.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.laatihan31.perkenalanmahasiswa;
+/**
+ *
+ * @author Rheiza
+ * NAMA  : RHEIZA AKMAL R
+ * KELAS : IF2
+ * NIM   : 10119061
+ */
+public class PBOIF210119061Laatihan31PerkenalanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      Mahasiswa mhs = new Mahasiswa ();
+        
+        mhs.nim = "10110269";
+        mhs.nama = "Rizki Adam Kurniawan";
+        mhs.perkenalkanDiri();
+        
+        mhs.nim = "10110270";
+        mhs.nama = "Indra Tiola";
+        mhs.perkenalkanDiri();
+
+        mhs.nim = "10110271";
+        mhs.nama = "Robi Tanzil Ganefi";
+        mhs.perkenalkanDiri();
+
+        mhs.nim = "10110269";
+        mhs.nama = "Muhammad Nur Awaluddin";
+        mhs.perkenalkanDiri();
+        
+    }
+    
+}
